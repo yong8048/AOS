76de55ab0937ee1c43bbfefab1fa75f6553d72fb
bwlee <lbw3973@naver.com>
2024-06-30 21:55:43 +0900

    feat: 네비게이션 설정

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavigationContainer, NavigationProp, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Google from "../pages/Google";
+import Kakao from "../pages/Kakao";
+import Home from "@/pages/Home";
+
+type StackParamList = {
+  Home: undefined;
+  Google: undefined;
+  Kakao: undefined;
+};
+
+export const useAppNavigation = (): NavigationProp<StackParamList> => {
+  return useNavigation<NavigationProp<StackParamList>>();
+};
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Google" component={Google} />
+        <Stack.Screen name="Kakao" component={Kakao} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
