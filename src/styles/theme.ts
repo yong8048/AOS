03f3b48c461adccb9c9f9fb30c2bd39f79c9bcc5
LeeByoungWook <lbw3973@naver.com>
2024-07-01 22:32:46 +0900

    feat: theme Provider 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+const color = {
+  primary_light: '#fff93b',
+  primary_normal: '#7b5339',
+  secondary_light: '#beaa9e',
+};
+
+const theme = {
+  color,
+};
+
+export type Theme = typeof theme;
+
+export default theme;
