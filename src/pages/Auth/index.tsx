f601f91fe01d352f1c9d6a43983f8b6884620bcd
LeeByoungWook <lbw3973@naver.com>
2024-07-03 16:32:11 +0900

    feat: Auth 관련 Navigation 설정

---
+++ src/pages/Auth/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Stack } from "@/navigation/Navigation";
+import Intro from "./Intro";
+import Login from "./Login";
+import SignUp from "./SignUp";
+
+const Auth = () => {
+  return (
+    <Stack.Navigator initialRouteName="Login" screenOptions={{ headerTitleAlign: "center" }}>
+      <Stack.Screen name="Intro" component={Intro} options={{ headerTitle: "LOGIN" }} />
+      <Stack.Screen name="Login" component={Login} options={{ headerTitle: "이메일 로그인" }} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
+};
+
+export default Auth;
