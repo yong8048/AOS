cffa44e9cd663e96d31cef1e29ea4fa43bf9c08f
LeeByoungWook <lbw3973@naver.com>
2024-07-03 16:31:44 +0900

    feat: navigation 기본 테마 설정

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { NavigationContainer, NavigationProp, useNavigation } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer, NavigationProp, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Google from "../pages/Google";
 import Kakao from "../pages/Kakao";
 import Home from "@/pages/Home";
 import Bible from "@/pages/Bible";
+import Auth from "@/pages/Auth";
+
+const navTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "#fff",
+  },
+};
 
 type StackParamList = {
   Home: undefined;
@@ -17,12 +26,13 @@ export const useAppNavigation = (): NavigationProp<StackParamList> => {
   return useNavigation<NavigationProp<StackParamList>>();
 };
 
-const Stack = createNativeStackNavigator();
+export const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+    <NavigationContainer theme={navTheme}>
+      <Stack.Navigator initialRouteName="Auth">
+        <Stack.Screen name="Auth" component={Auth} options={{ headerShown: false }} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Google" component={Google} />
         <Stack.Screen name="Kakao" component={Kakao} />
