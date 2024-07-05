ec4d01da5a34a1c359b0a2809fc6c796a3f31ed5
LeeByoungWook <lbw3973@naver.com>
2024-07-05 23:23:16 +0900

    feat: 커스텀 인풋 컴포넌트 props 인터페이스 정의

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,13 +1,26 @@
 import { KeyboardTypeOptions } from "react-native";
 
-interface IPropsTitleAndInput {
+interface IPropsTitleWithInput {
   children?: React.ReactNode;
   title: string;
   placeholder: string;
-  onChangeText: (text: string) => void;
+  onChangeText?: (text: string) => void;
+  onPress?: () => void;
+  value: string;
+  secure?: boolean;
+  keyboardType?: KeyboardTypeOptions;
+  disabled?: boolean;
+}
+
+interface IPropsTitleWithConfirmInput {
+  children?: React.ReactNode;
+  title: string;
+  placeholder: string;
+  onChangeText?: (text: string) => void;
+  onPress: () => void;
   value: string;
   secure?: boolean;
   keyboardType?: KeyboardTypeOptions;
 }
 
-export type { IPropsTitleAndInput };
+export type { IPropsTitleWithInput, IPropsTitleWithConfirmInput };
