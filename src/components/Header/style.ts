c9bf563fc09c103cfd673ade90bc02386677d140
LeeByoungWook <lbw3973@naver.com>
2024-07-15 13:54:18 +0900

    feat: 커스텀 헤더 컴포넌트 구현

---
+++ src/components/Header/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  padding: 20px 0;
+  align-items: center;
+  border-color: #bdbdbd;
+  border-bottom-width: 1px;
+`;
