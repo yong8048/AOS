e29a3fea3f154dd4b46d084321420fd7a7284a37
LeeByoungWook <lbw3973@naver.com>
2024-07-15 13:56:24 +0900

    feat: Main 페이지 추가

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { DefaultTheme, NavigationContainer, NavigationProp, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Google from "../pages/Google";
-import Kakao from "../pages/Kakao";
-import Home from "@/pages/Home";
-import Bible from "@/pages/Bible";
 import Auth from "@/pages/Auth";
+import Main from "@/pages/Main";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const navTheme = {
   ...DefaultTheme,
@@ -16,10 +14,7 @@ const navTheme = {
 };
 
 interface StackParamList {
-  Home: undefined;
-  Google: undefined;
-  Kakao: undefined;
-  Bible: undefined;
+  Main: undefined;
   Login: undefined;
   SignUp: undefined;
   Inquire: undefined;
@@ -32,16 +27,19 @@ export const useAppNavigation = (): NavigationProp<StackParamList> => {
 };
 
 export const Stack = createNativeStackNavigator();
+export const Tabs = createBottomTabNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer theme={navTheme}>
-      <Stack.Navigator initialRouteName="Auth">
+      <Stack.Navigator
+        initialRouteName="Auth"
+        screenOptions={{
+          contentStyle: { minWidth: 360, maxWidth: 720, width: "100%", margin: "auto" },
+        }}
+      >
         <Stack.Screen name="Auth" component={Auth} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Google" component={Google} />
-        <Stack.Screen name="Kakao" component={Kakao} />
-        <Stack.Screen name="Bible" component={Bible} />
+        <Stack.Screen name="Main" component={Main} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
