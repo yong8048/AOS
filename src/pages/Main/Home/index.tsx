17cb921d54c9931fd213d17d26e7835539e5696b
yong8048 <water8048@naver.com>
2024-09-11 00:53:51 +0900

    feat: 스크롤뷰 flatlist로 변경 및 스크롤 시 헤더 숨김 처리 추가

--- src/pages/Main/Home/index.tsx
+++ src/pages/Main/Home/index.tsx
@@ -1,13 +1,58 @@
 import React, { useState } from "react";
 import Post from "@/components/Main/Home/Post";
 import mockData from "@/mocks/mock_post.json";
-import { ScrollView } from "react-native";
+import { FlatList, Image, ScrollView, Text, View } from "react-native";
+import { IPostData } from "@/types/post";
+import * as S from "./style";
+const StickyHeader = () => (
+  <View
+    style={{
+      padding: 10,
+      backgroundColor: "#f8f8f8",
+      borderBottomWidth: 1,
+      borderBottomColor: "#fff",
+      flexDirection: "row",
+      justifyContent: "space-between",
+    }}
+  >
+    <Image source={require("@/assets/Icons/Main/Icon_DM.png")} style={{ marginTop: 8 }} />
+
+    <S.HeaderText>내게 능력 주시는자 안에서 내가 모든 것을 할 수 있느니라 (빌 4:13)</S.HeaderText>
+    <Image source={require("@/assets/Icons/Main/Icon_DM.png")} style={{ marginTop: 8 }} />
+  </View>
+);
 
 const Home = () => {
   const [isShowCommentsModal, setIsShowCommentsModal] = useState(false);
+  const [refresh, setRefresh] = useState(false);
+
+  const renderItem = ({ item }: { item: IPostData }) => (
+    <Post
+      postProps={item}
+      OpenCommentsModal={{
+        isOpenCommentsModal: isShowCommentsModal,
+        setIsOpenCommentsModal: setIsShowCommentsModal,
+      }}
+    />
+  );
+
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      {mockData.map((item, index) => (
+    <FlatList
+      data={mockData}
+      renderItem={renderItem}
+      keyExtractor={(item: IPostData) => item.id.toString()}
+      showsVerticalScrollIndicator={false}
+      stickyHeaderHiddenOnScroll={true}
+      stickyHeaderIndices={[0]}
+      ListHeaderComponent={<StickyHeader />}
+    />
+  );
+};
+
+export default Home;
+
+{
+  /* {mockData.map((item, index) => (
         <Post
           key={index}
           postProps={item}
@@ -16,9 +61,5 @@ const Home = () => {
             setIsOpenCommentsModal: setIsShowCommentsModal,
           }}
         />
-      ))}
-    </ScrollView>
-  );
-};
-
-export default Home;
+      ))} */
+}
---
+++ src/pages/Main/Home/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const HeaderText = styled.Text`
+  font-size: 12px;
+  font-weight: bold;
+  width: 80%;
+  padding: 5px;
+  background-color: #82dcff;
+  border-radius: 10px;
+`;
